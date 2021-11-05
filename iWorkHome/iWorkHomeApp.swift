5e93ece72edd97a0578b9a083acf0f518db78f11
David Buhauer <davbu@eg.dk>
2021-11-05 11:22:51 +0100

    Init project

---
+++ iWorkHome/iWorkHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWorkHomeApp.swift
+//  iWorkHome
+//
+//  Created by David Buhauer on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct iWorkHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
